67d13acfdd4ceac15282db43d0dd239b3b3beee4
Karthikeyan <jkarthionline@gmail.com>
2018-12-15 18:08:30 +0530

    auto login and .htaccess issues fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,22 +13,22 @@
    <!--  <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet" type="text/css">
     <!-- icon -->
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('public/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('public/img/favicon.png') }}">
 
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     
     <!-- CSS Files -->
-    <link href="{{ asset('css/material-dashboard.css?v=2.1.0') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/css/material-dashboard.css?v=2.1.0') }}" rel="stylesheet" type="text/css">
     
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{ asset('demo/demo.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/demo/demo.css') }}" rel="stylesheet" type="text/css">
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
@@ -96,23 +96,23 @@
 
   <!--   Core JS Files   -->
   
-  <script src="{{ asset('js/core/jquery.min.js') }} " type="text/javascript"></script>
-  <script src="{{ asset('js/core/popper.min.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/core/jquery.min.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/core/popper.min.js') }} " type="text/javascript"></script>
   
-  <script src="{{ asset('js/core/bootstrap-material-design.min.js') }} " type="text/javascript"></script>
-  <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/core/bootstrap-material-design.min.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/plugins/perfect-scrollbar.jquery.min.js') }} " type="text/javascript"></script>
 
   <!-- Chartist JS -->
-  <script src="{{ asset('js/plugins/chartist.min.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/plugins/chartist.min.js') }} " type="text/javascript"></script>
   
   <!--  Notifications Plugin  -->  
-  <script src="{{ asset('js/plugins/bootstrap-notify.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/plugins/bootstrap-notify.js') }} " type="text/javascript"></script>
   
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset('js/material-dashboard.min.js?v=2.1.0') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/js/material-dashboard.min.js?v=2.1.0') }} " type="text/javascript"></script>
 
   <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-  <script src="{{ asset('demo/demo.js') }} " type="text/javascript"></script>
+  <script src="{{ asset('public/demo/demo.js') }} " type="text/javascript"></script>
 
   <script>
     $(document).ready(function() {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,80 +10,80 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    
-    return redirect('login');
-});
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@indexs')->name('home');
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::post('/home', 'HomeController@index')->name('home');
+	Route::get('/', function () {	    
+	    Auth::login(User::find(1));
+    	return redirect('home');
+	});
 
 
-/* PROJECTS ROUTES*/
-Auth::routes();
+	Auth::routes();
 
-Route::get('create-projects', 'ProjectsController@create')->name('create-projects');
+	Route::get('/home', 'HomeController@indexs')->name('home');
 
-Route::post('store-projects', 'ProjectsController@store')->name('store-projects');
+	Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('projects', 'ProjectsController@index')->name('projects');
-
-Route::get('details-projects/{id}', 'ProjectsController@show')->name('details-projects');
-Route::post('details-projects/{id}', 'ProjectsController@show')->name('details-projects');
-
-Route::get('projects/{id}', 'ProjectsController@edit')->name('projects/{id}');
-
-Route::post('update-projects', 'ProjectsController@update')->name('update-projects');
-
-Route::get('delete-projects/{id}', 'ProjectsController@delete')->name('delete-projects/{id}');
+	Route::post('/home', 'HomeController@index')->name('home');
 
 
+	/* PROJECTS ROUTES*/
+	Auth::routes();
 
-/* PROJECTS ROUTES*/
+	Route::get('create-projects', 'ProjectsController@create')->name('create-projects');
 
-Auth::routes();
+	Route::post('store-projects', 'ProjectsController@store')->name('store-projects');
 
-Route::get('create-workhours', 'WorkhoursController@create')->name('create-workhours');
+	Route::get('projects', 'ProjectsController@index')->name('projects');
 
-Route::post('store-workhours', 'WorkhoursController@store')->name('store-workhours');
+	Route::get('details-projects/{id}', 'ProjectsController@show')->name('details-projects');
+	Route::post('details-projects/{id}', 'ProjectsController@show')->name('details-projects');
 
-Route::get('workhours', 'WorkhoursController@index')->name('workhours');
-Route::post('workhours', 'WorkhoursController@index')->name('workhours');
+	Route::get('projects/{id}', 'ProjectsController@edit')->name('projects/{id}');
 
-Route::get('workhours/{id}', 'WorkhoursController@edit')->name('workhours/{id}');
+	Route::post('update-projects', 'ProjectsController@update')->name('update-projects');
 
-Route::post('update-workhours', 'WorkhoursController@update')->name('update-workhours');
-
-Route::get('delete-workhours/{id}', 'WorkhoursController@delete')->name('delete-workhours/{id}');
-
-Route::get('details-workhours', 'WorkhoursController@show')->name('details-workhours');
+	Route::get('delete-projects/{id}', 'ProjectsController@delete')->name('delete-projects/{id}');
 
 
-/* RESOURCE ROUTES*/
 
-Auth::routes();
+	/* PROJECTS ROUTES*/
 
-Route::get('create-resource', 'ResourceController@create')->name('create-resource');
+	Auth::routes();
 
-Route::post('store-resource', 'ResourceController@store')->name('store-resource');
+	Route::get('create-workhours', 'WorkhoursController@create')->name('create-workhours');
 
-Route::get('index-resource', 'ResourceController@index')->name('index-resource');
+	Route::post('store-workhours', 'WorkhoursController@store')->name('store-workhours');
 
-Route::get('edit-resource/{id}', 'ResourceController@edit')->name('edit-resource/{id}');
+	Route::get('workhours', 'WorkhoursController@index')->name('workhours');
+	Route::post('workhours', 'WorkhoursController@index')->name('workhours');
 
-Route::post('update-resource', 'ResourceController@update')->name('update-resource');
+	Route::get('workhours/{id}', 'WorkhoursController@edit')->name('workhours/{id}');
 
-Route::get('delete-resource/{id}', 'ResourceController@delete')->name('delete-resource/{id}');
+	Route::post('update-workhours', 'WorkhoursController@update')->name('update-workhours');
 
-Route::get('details-resource/{id}', 'ResourceController@show')->name('details-resource');
+	Route::get('delete-workhours/{id}', 'WorkhoursController@delete')->name('delete-workhours/{id}');
 
-Route::post('details-resource/{id}', 'ResourceController@show')->name('details-resource');
+	Route::get('details-workhours', 'WorkhoursController@show')->name('details-workhours');
+
+
+	/* RESOURCE ROUTES*/
+
+	Auth::routes();
+
+	Route::get('create-resource', 'ResourceController@create')->name('create-resource');
+
+	Route::post('store-resource', 'ResourceController@store')->name('store-resource');
+
+	Route::get('index-resource', 'ResourceController@index')->name('index-resource');
+
+	Route::get('edit-resource/{id}', 'ResourceController@edit')->name('edit-resource/{id}');
+
+	Route::post('update-resource', 'ResourceController@update')->name('update-resource');
+
+	Route::get('delete-resource/{id}', 'ResourceController@delete')->name('delete-resource/{id}');
+
+	Route::get('details-resource/{id}', 'ResourceController@show')->name('details-resource');
+
+	Route::post('details-resource/{id}', 'ResourceController@show')->name('details-resource');
+
 
 
